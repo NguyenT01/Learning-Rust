b40f93efb49c2ce1367c5c0477cef3eaaa2e7e50
NguyenT01 <transinguyen2001@gmail.com>
2023-06-28 23:04:43 +0700

    Chapter 2 create

---
+++ 2/guessing_game/target/debug/deps/guessing_game-78d5cbb2e73fba48.d
@@ -0,0 +1,5 @@
+C:\Users\trans\Máy tính\rust\Learning-Rust\2\guessing_game\target\debug\deps\guessing_game-78d5cbb2e73fba48.rmeta: src\main.rs
+
+C:\Users\trans\Máy tính\rust\Learning-Rust\2\guessing_game\target\debug\deps\guessing_game-78d5cbb2e73fba48.d: src\main.rs
+
+src\main.rs:
---
+++ 2/guessing_game/target/debug/deps/guessing_game-8e27d097ed89a997.d
@@ -0,0 +1,5 @@
+C:\Users\trans\Máy tính\rust\Learning-Rust\2\guessing_game\target\debug\deps\guessing_game-8e27d097ed89a997.rmeta: src\main.rs
+
+C:\Users\trans\Máy tính\rust\Learning-Rust\2\guessing_game\target\debug\deps\guessing_game-8e27d097ed89a997.d: src\main.rs
+
+src\main.rs:
